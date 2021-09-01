d425099918c97270a309b0621c80c145ab7a7cd6
Simon Lee <simon.lee@cardinalblue.com>
2021-09-01 13:26:38 +0800

    Render table for large width screens

--- components/history.js
+++ components/history.js
@@ -11,22 +11,16 @@ import {
   HStack,
   VStack,
   Heading,
-  Wrap
+  Wrap,
+  useBreakpointValue
 } from '@chakra-ui/react'
 
 export default function History({ data }) {
+  const variant = useBreakpointValue({ base: "list", sm: "list", md: "list", lg: "table"})
+
   if (data == null) {
     return null;
   }
-  
-  const columns = [
-    {key: "transactionId", displayName: "Transaction ID"}, 
-    {key: "productId", displayName: "Product"},
-    {key: "purchaseDate", displayName: "Purchase Date"},
-    {key: "expiresDate", displayName: "Expire Date"},
-    {key: "type", displayName: "Product Type"},
-    {key: "inAppOwnershipType", displayName: "Ownership Type"},
-  ];
 
   data.signedTransactions.forEach((transaction) => {
     transaction.purchaseDate = new Date(transaction.purchaseDate).toLocaleDateString();
@@ -38,60 +32,75 @@ export default function History({ data }) {
       <Box align="center">
         <Heading py="4" size="xl">History</Heading>
       </Box>
-      <VStack align="center">
-        {
-          data.signedTransactions.map((transaction) => {
-            return (
-              <Box align="start" p="4" w={ ["sm","md"] } borderWidth="1px" borderRadius="lg" key={ transaction.transactionId } overflow="wrap">
-                <Wrap justify="space-between">
-                  <Badge fontSize={ ["xs", "sm"] }>{ transaction.type }</Badge>
-                  <Text fontWeight="light" fontSize={ ["xs", "sm"] } color="gray.800">#{ transaction.transactionId }</Text>
-                </Wrap>
-                <Text py="2" fontSize={ ["sm", "md"] } fontWeight="medium">{ transaction.productId }</Text>
-                <HStack>
-                  <Text fontSize={ ["xs", "sm"] }>Purchased at:</Text>
-                  <Text fontSize={ ["xs", "sm"] }>{ transaction.purchaseDate }</Text>
-                </HStack>
-                <HStack>
-                  <Text fontSize={ ["xs", "sm"] }>Expires at:</Text>
-                  <Text fontSize={ ["xs", "sm"] }>{ transaction.expiresDate }</Text>
-                </HStack>
-              </Box>
-            )
-          })
-        }
-      </VStack>
+      { variant === "table" ? renderTable(data) : renderList(data) }
     </div>
   );
+}
 
+function renderTable(data) {
+
+  const columns = [
+    {key: "transactionId", displayName: "Transaction ID"}, 
+    {key: "productId", displayName: "Product"},
+    {key: "purchaseDate", displayName: "Purchase Date"},
+    {key: "expiresDate", displayName: "Expire Date"},
+    {key: "type", displayName: "Product Type"},
+    {key: "inAppOwnershipType", displayName: "Ownership Type"},
+  ];
+  
   return (
-    <div>
-      <Text py="4" fontSize="4xl">History</Text>
-      <Table variant="simple" size="sm">
-        <Thead>
-          <Tr>
-            {
-              columns.map((column, index) => {
-                return <Th key={ index }>{ column.displayName }</Th>
-              })
-            }
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.signedTransactions.map((transaction) => {
-            return (
-              <Tr key={ transaction.transactionId }>
-                {
-                  columns.map((column) => {
-                    return <Td key={ column.key }>{ transaction[column.key] }</Td>
-                  })
-                }
-              </Tr>
-            ) 
-          })}
-          
-        </Tbody>
-      </Table>
-    </div>
+    <Table variant="simple" size="sm">
+      <Thead>
+        <Tr>
+          {
+            columns.map((column, index) => {
+              return <Th key={ index }>{ column.displayName }</Th>
+            })
+          }
+        </Tr>
+      </Thead>
+      <Tbody>
+        {data.signedTransactions.map((transaction) => {
+          return (
+            <Tr key={ transaction.transactionId }>
+              {
+                columns.map((column) => {
+                  return <Td key={ column.key }>{ transaction[column.key] }</Td>
+                })
+              }
+            </Tr>
+          ) 
+        })}
+        
+      </Tbody>
+    </Table>
+  )
+}
+
+function renderList(data) {
+  return (
+    <VStack align="center">
+      {
+        data.signedTransactions.map((transaction) => {
+          return (
+            <Box align="start" p="4" w={ ["sm","md"] } borderWidth="1px" borderRadius="lg" key={ transaction.transactionId } overflow="wrap">
+              <Wrap justify="space-between">
+                <Badge fontSize={ ["xs", "sm"] }>{ transaction.type }</Badge>
+                <Text fontWeight="light" fontSize={ ["xs", "sm"] } color="gray.800">#{ transaction.transactionId }</Text>
+              </Wrap>
+              <Text py="2" fontSize={ ["sm", "md"] } fontWeight="medium">{ transaction.productId }</Text>
+              <HStack>
+                <Text fontSize={ ["xs", "sm"] }>Purchased at:</Text>
+                <Text fontSize={ ["xs", "sm"] }>{ transaction.purchaseDate }</Text>
+              </HStack>
+              <HStack>
+                <Text fontSize={ ["xs", "sm"] }>Expires at:</Text>
+                <Text fontSize={ ["xs", "sm"] }>{ transaction.expiresDate }</Text>
+              </HStack>
+            </Box>
+          )
+        })
+      }
+    </VStack>
   )
 }
