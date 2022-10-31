bc97036449f33b18b5380418ccd76a8b6355c17e
Simon Lee <simon.lee@cardinalblue.com>
2022-10-31 15:50:05 +0800

    feat: simplify app store key processing

--- pages/api/transactions/[id].js
+++ pages/api/transactions/[id].js
@@ -3,14 +3,14 @@ import StoreKit from 'storekit-sdk';
 
 export default async (req, res) => {
   const { id, env } = req.query;
-  const key = JSON.parse(process.env.APP_STORE_KEY);
+  const key = process.env.APP_STORE_KEY;
   const baseURL = env === 'production' ? 'https://api.storekit.itunes.apple.com/inApps/v1' : 'https://api.storekit-sandbox.itunes.apple.com/inApps/v1'
   const store = new StoreKit({
     baseURL: baseURL,
     issuer: '69a6de70-ff9b-47e3-e053-5b8c7c11a4d1',
     bid: 'com.cardinalblue.PicCollage',
     kid: '6R27A8XG4T',
-    privateKey: key.key, 
+    privateKey: key,
   });
 
   try {
