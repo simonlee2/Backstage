42c254537c74de231358c54988baca0df608a970
Simon Lee <simon.lee@cardinalblue.com>
2023-04-19 17:01:58 +0800

    feat: use router to update query and automatically search

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,89 @@
 import NextLayout from '../components/layout'
-import styles from '../styles/Home.module.css'
 import {
   Text,
   Input,
   Container,
   Button,
-  Select,
   Flex
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Subscriptions from '../components/subscriptions'
 import History from '../components/history'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const [query, setQuery] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [transactionId, setTransactionId] = useState(null);
   const [subscriptions, setSubscriptions] = useState(null);
   const [history, setHistory] = useState(null);
+  const router = useRouter();
+  
+  useEffect(async () => {
+    const urlSearchParams = new URLSearchParams(window.location.search)  
+    const query = urlSearchParams.get('query')
+    setQuery(query);
+
+    try {
+      const tid = await convertToTransactionId(query);
+      setTransactionId(tid);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (transactionId != null) {
+      handleTIDSearch(transactionId);
+    }
+  }, [transactionId]);
 
   const handleSearch = async event => {
     event.preventDefault();
     setIsLoading(true);
 
-    const query = event.target.query.value;
+    router.push(`/?query=${query}`);
+
     try {
-      if (isCBID(query)) {
-        const tid = await fetchTransactionId(query);
-        await handleTIDSearch(tid);
-      } else if (isTransactionId(query)) {
-        await handleTIDSearch(query);
-      } else {
-        const error = new Error("Unknown query format");
-        console.error(error);
-        alert(error);
-      }
+      const tid = await convertToTransactionId(query);
+      setTransactionId(tid);
     } catch (error) {
       console.error(error);
       alert(error);
     }
     
     setIsLoading(false);
+  }
+
+  const convertToTransactionId = async (query) => {
+    if (isCBID(query)) {
+      return await fetchTransactionId(query);
+    } else if (isTransactionId(query)) {
+      return query;
+    } else {
+      throw new Error("Unknown query format");
+    }
+  }
+
+  const handleTIDSearch = async (tid) => {
+    try {
+      const { subscriptions } = await fetchSubscriptions(tid);
+      setSubscriptions(subscriptions);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+      setSubscriptions(null);
+    }
+
+    try {
+      const { history } = await fetchTransactions(tid);
+      setHistory(history);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+      setSubscriptions(null);
+    }
   }
 
   const isCBID = (query) => {
@@ -111,27 +155,6 @@ export default function Home() {
     }
   }
 
-  const handleTIDSearch = async (tid) => {
-
-    try {
-      const { subscriptions } = await fetchSubscriptions(tid);
-      setSubscriptions(subscriptions);
-    } catch (error) {
-      console.error(error);
-      alert(error);
-      setSubscriptions(null);
-    }
-
-    try {
-      const { history } = await fetchTransactions(tid);
-      setHistory(history);
-    } catch (error) {
-      console.error(error);
-      alert(error);
-      setSubscriptions(null);
-    }
-  }
-
   return (
     <NextLayout>
       <Container
@@ -148,7 +171,17 @@ export default function Home() {
           {/* Search with CBID */}
           <form onSubmit={handleSearch}>
             <Flex direction={['column', 'row']} spacing="5px" py="4" align="center">
-              <Input id="query" w={ ["md", "md", "lg"]} name="query" type="search" placeholder="Transaction ID or CBID" required />
+              <Input
+                id="query"
+                w={ ["md", "md", "lg"]} 
+                name="query" 
+                value={query}
+
+                onChange={e => setQuery(e.target.value)}
+                type="search" 
+                placeholder="Transaction ID or CBID" 
+                required 
+              />
               <Button w="120px" isLoading={ isLoading } type="submit" colorScheme="green">Search</Button>
             </Flex>
           </form>
